1833cfe901dd878bde62fc2d98de40d83d5d38f7
Emanuell95 <emanueltute95@gmail.com>
2025-03-27 18:49:33 -0400

    3-default-parameters0.4

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,3 +1,4 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = 89) {
-  return initialNumber + expansion1989;
+// This code demonstrates the use of default parameters in a function.
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
 }
