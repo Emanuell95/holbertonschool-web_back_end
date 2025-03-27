4c857b443232969347b3ecf11fc4da0d8a38ead9
Emanuell95 <emanueltute95@gmail.com>
2025-03-27 19:18:14 -0400

    Getting full budget0.1

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,5 +1,4 @@
-// This file exports a function that takes three parameters: income, gdp, and capita.
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
