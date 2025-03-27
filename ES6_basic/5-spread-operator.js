ad1a15ff6eee2c06ea95778e767d25b704118e95
Emanuell95 <emanueltute95@gmail.com>
2025-03-27 19:08:15 -0400

    spread operator

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,4 @@
+// This code demonstrates the use of the spread operator in a function.
 export default function concatArrays(array1, array2, string) {
     return [...array1, ...array2, ...string];
   }
