8a08c699ae50c98372e3f7738fdda0a8a6b6cb8a
Emanuell95 <emanueltute95@gmail.com>
2025-03-27 19:14:47 -0400

    geting budget

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,14 +1,15 @@
+// This file contains a function that takes in three parameters: income, gdp, and capita.
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {
-  
-    [`income-${getCurrentYear()}`]:income,
-    [`gdp-${getCurrentYear()}`]:gdp,
-    [`capita-${getCurrentYear()}`]:capita
-    };
-    return budget;
-  }
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
