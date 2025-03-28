a533455274be0a1551ad52ad9f6e8bc8dab2325a
Emanuell95 <emanueltute95@gmail.com>
2025-03-28 15:41:43 -0400

    hbtn_course0.1

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,19 @@
 class HolbertonCourse {
     constructor(name, length, students) {
+      // Type checking in the constructor
+      if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      if (typeof length !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
+      if (!Array.isArray(students)) {
+        throw new TypeError('Students must be an Array');
+      }
+      if (!students.every(student => typeof student === 'string')) {
+        throw new TypeError('Students must be an array of strings');
+      }
+
       this._name = name;
       this._length = length;
       this._students = students;
@@ -37,11 +51,13 @@ class HolbertonCourse {
      * @param {Array} students
      */
     set students(students) {
-      if (students instanceof Array) {
-        this._students = students;
-      } else {
+      if (!Array.isArray(students)) {
         throw new TypeError('Students must be an Array');
       }
+      if (!students.every(student => typeof student === 'string')) {
+        throw new TypeError('Students must be an array of strings');
+      }
+      this._students = students;
     }
   
     get students() {
