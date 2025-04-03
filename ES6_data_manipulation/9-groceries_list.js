c333f08677663f32cd1298c1e3a83cebb25e73a7
Emanuell95 <emanueltute95@gmail.com>
2025-04-02 20:02:09 -0400

    Map data structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default groceriesList;
+function groceriesList() {
+    return new Map([
+        ['Apples', 10],
+        ['Tomatoes', 10],
+        ['Pasta', 1],
+        ['Rice', 1],
+        ['Banana', 5]
+    ]);
+}
