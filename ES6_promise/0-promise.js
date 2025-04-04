449b684e6985cf1f48e0f3e0a20ab78db18ab8eb
Emanuell95 <emanueltute95@gmail.com>
2025-04-04 13:55:30 -0400

    keep promises that you can keep

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,9 @@
 function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve('Promise resolved');
-      }, 1000);
-    });
-  }
-  export default getResponseFromAPI;
-  
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Promise resolved');
+    }, 1000);
+  });
+}
 
+export default getResponseFromAPI;
