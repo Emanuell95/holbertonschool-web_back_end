1107a8ad1d54a8ade6190fcd2f24a7f9cefce257
Emanuell95 <emanueltute95@gmail.com>
2025-04-04 14:07:41 -0400

    Promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,17 +1,16 @@
-//Working with Promise resolve and reject
-export default function getResponseFromAPI(success) {
-{
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success'
-            });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
-}
-}
+// Working with Promise resolve and rejects
 
+
+export default function getResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
 
