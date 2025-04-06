c849af6bed812dec5aaab2993798664d45c87e02
Emanuell95 <emanueltute95@gmail.com>
2025-04-06 12:09:15 -0400

    3-get_ids_sum.js

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,3 @@
-function getStudentIdsSum(getListStudents) {
-    return getListStudents.reduce((acc, student) => acc + student.id, 0);
-}
-export default getStudentIdsSum;
+export default function getStudentIdsSum(students) {
+    return students.reduce((sum, student) => sum + student.id, 0);
+  }
