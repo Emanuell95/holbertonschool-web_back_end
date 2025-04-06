94caf84b9be3526cfbfce78582d174bdfc25ba4f
Emanuell95 <emanueltute95@gmail.com>
2025-04-06 11:51:45 -0400

    1-promise.js0.1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,16 +1,12 @@
-// Working with Promise resolve and rejects
-
-
-export default function getResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({
-          status: 200,
-          body: 'Success',
-        });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-  }
-
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
