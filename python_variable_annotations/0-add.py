fa09b74e266c6049117a06b17d49993f7303cdcc
Emanuell95 <emanueltute95@gmail.com>
2025-04-25 15:55:55 -0400

    basic annotations-add0.3

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
-def add (a: float, b: float) -> float:
-    """Return the sum of a and b."""
+"""Module that provides a function to add two floating point numbers."""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of a and b.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
