8111d3fbb71f2051b5cd858b47de48af7d2417ad
Emanuell95 <emanueltute95@gmail.com>
2025-04-25 16:06:52 -0400

    Concatenating

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A script that concatenates two strings."""
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of str1 and str2.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenation of str1 and str2.
+    """
+    return str1 + str2
