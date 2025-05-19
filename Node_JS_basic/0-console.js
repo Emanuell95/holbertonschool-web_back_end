91e6a1a19d763602d7e979d31bd442a95adc1dc5
Emanuell95 <emanueltute95@gmail.com>
2025-05-19 19:28:48 -0400

    0-console0.2

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,5 @@
-const displayMessage = (message) => {
-  // This function will display a message in the console
+function displayMessage(message) {
   console.log(message);
 }
 
 module.exports = displayMessage;
-
